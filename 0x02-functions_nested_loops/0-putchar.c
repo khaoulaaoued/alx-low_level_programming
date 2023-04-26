0a791832190fc2bb1fe5227056f349c669491f96
khaoulaaoued <teste@gmail.com>
2023-04-26 14:50:22 -0700

    helooc

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,17 @@
 #include "main.h"
- /**
- *main - Entry point
- *Description: 'prints _putchar to stdout'
- *Return: always 0
- */
+/**
+* main - Entry point
+*
+* Return: Always 0(success)
+*/
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
 	_putchar('\n');
 
 	return (0);
